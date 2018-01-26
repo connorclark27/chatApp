976abc43d1e68dd432a319bb3d73c00f7ab40517
Clark <c_clark20@tss-hr420-dm14.fc.ca>
2018-01-26 10:52:33 -0500

    added portfolio and contact

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', (req, res)=> {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/contact', (req, res)=> {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/portfolio', (req, res)=> {
+  res.sendFile(__dirname + '/portfolio.html');
+});
+
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
