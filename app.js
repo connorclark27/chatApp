3fa86aea319a3373cddca8500478a59d75a4071b
Clark <c_clark20@tss-hr420-dm14.local>
2018-02-02 09:39:48 -0500

    working structure

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express'); //include this just like a php include
 const app = express();
 
-app.get('/', (req, res)=> {
-  res.sendFile(__dirname + '/index.html');
-});
 
-app.get('/contact', (req, res)=> {
+//app routes
+app.use(require('./routes/index'));
+app.use(require('./routes/contact'));
+app.use(require('./routes/portfolio'));
+
+
+/*app.get('/contact', (req, res)=> {
   res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/portfolio', (req, res)=> {
   res.sendFile(__dirname + '/portfolio.html');
-});
+});*/
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
